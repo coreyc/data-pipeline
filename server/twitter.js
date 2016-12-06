353947fbc1ea2a7944a0f5db1fa44f3a3e99f67f
coreyc <ccleary00@gmail.com>
2016-12-05 22:39:42 -0600

    Working

--- server/twitter.js
+++ server/twitter.js
@@ -1,15 +1,19 @@
 const Twitter = require('twitter');
+require('dotenv').config();
  
 const client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
- 
-const params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, (error, tweets, response) => {
-  if (!error) {
-    console.log(tweets);
-  }
+
+client.stream('statuses/filter', {track: 'twitter'},  function(stream) {
+  stream.on('data', function(tweet) {
+    console.log(tweet.text);
+  });
+
+  stream.on('error', function(error) {
+    console.log(error);
+  });
 });
